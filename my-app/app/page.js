67fb332f175945b3d9ee757d532dc81466d44741
AMB <a.michael.boyle@gmail.com>
2023-11-20 23:40:04 -0500

    removed kanban board data to utils folder to clean up the component

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,76 +1,17 @@
 import styles from './page.module.css';
 import Column from "@/components/column/column";
+import { board, list } from '@/utils/kanban-data';
 
 export default function Home() {
-  const board = ['Backlog', 'Doing', 'Review', 'Done'];
-
-  const list = {
-    'Backlog': [
-      {
-        title: 'test1',
-        description: 'some random task to do'
-      },
-      {
-        title: 'test2',
-        description: 'some random task to do'
-      },
-      {
-        title: 'test3',
-        description: 'some random task to do'
-      }
-    ],
-    'Doing': [
-      {
-        title: 'test4',
-        description: 'some random task to do'
-      },
-      {
-        title: 'test5',
-        description: 'some random task to do'
-      },
-      {
-        title: 'test6',
-        description: 'some random task to do'
-      }
-    ],
-    'Review': [
-      {
-        title: 'test1',
-        description: 'some random task to do'
-      },
-      {
-        title: 'test2',
-        description: 'some random task to do'
-      },
-      {
-        title: 'test3',
-        description: 'some random task to do'
-      }
-    ],
-    'Done': [
-      {
-        title: 'test1',
-        description: 'some random task to do'
-      },
-      {
-        title: 'test2',
-        description: 'some random task to do'
-      },
-      {
-        title: 'test3',
-        description: 'some random task to do'
-      }
-    ]
-  }
 
   return (
     <div className={styles.container}>
       <h1 className={styles.header}>Home Page</h1>
       <div className={styles.row}>
         {
-          board.map((title) => {
+          board.map((item) => {
             return (
-              <Column title={title} list={list[title]} />
+              <Column board={item.title} list={list[item.title]} />
             )
           })
         }
