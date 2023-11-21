02c12e79ea6cc15f3d85ece973d0e2d3e23c483d
AMB <a.michael.boyle@gmail.com>
2023-11-20 23:40:29 -0500

    renamed title prop to board for clairty

--- my-app/components/column/column.jsx
+++ my-app/components/column/column.jsx
@@ -1,10 +1,10 @@
 import styles from './column.module.css';
 
-export default function Column({ title, list }) {
+export default function Column({ board, list }) {
 
     return (
         <div className={styles.container}>
-            <h5 className={styles.header}>{title}</h5>
+            <h5 className={styles.header}>{board}</h5>
             <div className={styles.spacer}>
             {
                 list.map(task => {
