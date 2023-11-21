39549d9814b54f39ab969a3cb9d23346ae964868
AMB <a.michael.boyle@gmail.com>
2023-11-20 23:39:36 -0500

    moved kanban board data to utils folder

---
+++ my-app/utils/kanban-data.js
@@ -0,0 +1,60 @@
+export const board = [{ title: 'Backlog', color: 'rgba(247, 0, 0, 0.548)' }, { title: 'Doing' }, { title: 'Review' }, { title: 'Done' }];
+
+export const list = {
+    'Backlog': [
+        {
+            title: 'test1',
+            description: 'some random task to do'
+        },
+        {
+            title: 'test2',
+            description: 'some random task to do'
+        },
+        {
+            title: 'test3',
+            description: 'some random task to do'
+        }
+    ],
+    'Doing': [
+        {
+            title: 'test4',
+            description: 'some random task to do'
+        },
+        {
+            title: 'test5',
+            description: 'some random task to do'
+        },
+        {
+            title: 'test6',
+            description: 'some random task to do'
+        }
+    ],
+    'Review': [
+        {
+            title: 'test1',
+            description: 'some random task to do'
+        },
+        {
+            title: 'test2',
+            description: 'some random task to do'
+        },
+        {
+            title: 'test3',
+            description: 'some random task to do'
+        }
+    ],
+    'Done': [
+        {
+            title: 'test1',
+            description: 'some random task to do'
+        },
+        {
+            title: 'test2',
+            description: 'some random task to do'
+        },
+        {
+            title: 'test3',
+            description: 'some random task to do'
+        }
+    ]
+}
