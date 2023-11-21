f41900dbfbf5919fa324312df29fac5117b4e7f8
AMB <a.michael.boyle@gmail.com>
2023-11-20 23:20:35 -0500

    added texture to cards

--- my-app/components/column/column.jsx
+++ my-app/components/column/column.jsx
@@ -2,20 +2,21 @@ import styles from './column.module.css';
 
 export default function Column({ title, list }) {
 
-
     return (
         <div className={styles.container}>
             <h5 className={styles.header}>{title}</h5>
+            <div className={styles.spacer}>
             {
                 list.map(task => {
                     return (
-                        <div>
-                            <div>{task.title}</div>
-                            <p>{task.description}</p>
+                        <div className={styles.card}>
+                            <div className={styles.title}>{task.title}</div>
+                            <p className={styles.description}>{task.description}</p>
                         </div>
                     )
                 })
             }
+            </div>
         </div>
     )
 }
