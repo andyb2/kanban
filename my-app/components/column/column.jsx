cc5a32d4f30ab6db8fa96391142a1e8fd9982c0e
AMB <a.michael.boyle@gmail.com>
2023-11-21 22:58:40 -0500

    modified the way state was being changed. Doesn't cause double renders now

--- my-app/components/column/column.jsx
+++ my-app/components/column/column.jsx
@@ -2,12 +2,12 @@
 import styles from './column.module.css';
 import { useState, useRef } from 'react';
 
-export default function Column({ board, list, setListData, setDraggedCard }) {
+export default function Column({ board, list, setListData, listData, setDraggedCard, draggedCard }) {
     const cardRef = useRef(null);
     const columnRef = useRef(null)
     let dragged = false;
 
-    const handleOnDrag = (event, card, idx, title) => {
+    const handleOnDrag = (event, card, title) => {
         const { target } = event;
         
         // Waits when the dragging of a card has started then triggers the display none
@@ -16,25 +16,29 @@ export default function Column({ board, list, setListData, setDraggedCard }) {
             target.style.display = "none";
         }, 0) 
         
-        setDraggedCard(card);
+        setDraggedCard({ title, card });
     }
 
     const handleDragOver = (e) => {
         e.preventDefault();
     }
 
-    const handleOnDrop = (e) => {
+    const handleOnDrop = (e, boardTitle) => {
         e.preventDefault();
+        const list = { ...listData };
+        list[draggedCard.title] = list[draggedCard.title].filter((task) => task.id !== draggedCard.card.id);
+        list[boardTitle].push(draggedCard.card);
+        setListData(list);
     }
-
+    
     return (
-        <div className={styles.container} onDragOver={(e) => handleDragOver(e)} onDrop={(e) => handleOnDrop(e)} data-board={board.title} ref={columnRef}>
+        <div className={styles.container} onDragOver={(e) => handleDragOver(e)} onDrop={(e) => handleOnDrop(e, board.title)} data-board={board.title} ref={columnRef}>
             <h5 className={styles.header} style={{background: board.color}}>{board.title}</h5>
             <div className={styles.spacer}>
             {
-                list.map((task, idx) => {
+                list.map((task) => {
                     return (
-                        <div className={styles.card} draggable={true} onDragStart={(e) => handleOnDrag(e, task, idx, board.title)}>
+                        <div className={styles.card} draggable={true} onDragStart={(e) => handleOnDrag(e, task, board.title)}>
                             <div className={styles.title}>{task.title}</div>
                             <p className={styles.description}>{task.description}</p>
                         </div>
