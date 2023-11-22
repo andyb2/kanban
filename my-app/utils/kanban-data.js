222ecc8b4d2c8a558e3791415caea28c93fe1ba2
AMB <a.michael.boyle@gmail.com>
2023-11-21 22:59:02 -0500

    added ids to kanban list

--- my-app/utils/kanban-data.js
+++ my-app/utils/kanban-data.js
@@ -3,42 +3,51 @@ export const board = [{ title: 'Backlog', color: 'rgba(247, 0, 0, 0.548)' }, { t
 export const list = {
     'Backlog': [
         {
+            id: '1',
             title: 'test1',
             description: 'some random task to do'
         },
         {
+            id: '2',
             title: 'test2',
             description: 'some random task to do'
         },
         {
+            id: '3',
             title: 'test3',
             description: 'some random task to do'
         }
     ],
     'Doing': [
         {
+            id: '4',
             title: 'test4',
             description: 'some random task to do'
         },
         {
+            id: '5',
             title: 'test5',
             description: 'some random task to do'
         },
         {
+            id: '6',
             title: 'test6',
             description: 'some random task to do'
         }
     ],
     'Review': [
         {
+            id: '7',
             title: 'test1',
             description: 'some random task to do'
         },
         {
+            id: '8',
             title: 'test2',
             description: 'some random task to do'
         },
         {
+            id: '9',
             title: 'test3',
             description: 'some random task to do'
         }
