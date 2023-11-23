10b3cca47a074950dddb2293933657d8fb6ef0a5
AMB <a.michael.boyle@gmail.com>
2023-11-22 22:30:00 -0500

    prettier formatting

--- my-app/utils/kanban-data.js
+++ my-app/utils/kanban-data.js
@@ -1,58 +1,61 @@
-export const board = [{ title: 'Backlog', color: 'rgba(247, 0, 0, 0.548)' }, { title: 'Doing', color: 'rgba(247, 243, 0, 0.548)' }, { title: 'Review', color: 'rgba(58, 247, 0, 0.548)' }, { title: 'Done', color: 'rgba(0, 231, 247, 0.548)' }];
+export const board = [
+  { title: "Backlog", color: "rgba(247, 0, 0, 0.548)" },
+  { title: "Doing", color: "rgba(247, 243, 0, 0.548)" },
+  { title: "Review", color: "rgba(58, 247, 0, 0.548)" },
+  { title: "Done", color: "rgba(0, 231, 247, 0.548)" },
+];
 
 export const list = {
-    'Backlog': [
-        {
-            id: '1',
-            title: 'test1',
-            description: 'some random task to do'
-        },
-        {
-            id: '2',
-            title: 'test2',
-            description: 'some random task to do'
-        },
-        {
-            id: '3',
-            title: 'test3',
-            description: 'some random task to do'
-        }
-    ],
-    'Doing': [
-        {
-            id: '4',
-            title: 'test4',
-            description: 'some random task to do'
-        },
-        {
-            id: '5',
-            title: 'test5',
-            description: 'some random task to do'
-        },
-        {
-            id: '6',
-            title: 'test6',
-            description: 'some random task to do'
-        }
-    ],
-    'Review': [
-        {
-            id: '7',
-            title: 'test1',
-            description: 'some random task to do'
-        },
-        {
-            id: '8',
-            title: 'test2',
-            description: 'some random task to do'
-        },
-        {
-            id: '9',
-            title: 'test3',
-            description: 'some random task to do'
-        }
-    ],
-    'Done': [
-
-    ]
-}
+  Backlog: [
+    {
+      id: "1",
+      title: "test1",
+      description: "some random task to do",
+    },
+    {
+      id: "2",
+      title: "test2",
+      description: "some random task to do",
+    },
+    {
+      id: "3",
+      title: "test3",
+      description: "some random task to do",
+    },
+  ],
+  Doing: [
+    {
+      id: "4",
+      title: "test4",
+      description: "some random task to do",
+    },
+    {
+      id: "5",
+      title: "test5",
+      description: "some random task to do",
+    },
+    {
+      id: "6",
+      title: "test6",
+      description: "some random task to do",
+    },
+  ],
+  Review: [
+    {
+      id: "7",
+      title: "test1",
+      description: "some random task to do",
+    },
+    {
+      id: "8",
+      title: "test2",
+      description: "some random task to do",
+    },
+    {
+      id: "9",
+      title: "test3",
+      description: "some random task to do",
+    },
+  ],
+  Done: [],
+};
