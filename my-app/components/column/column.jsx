649b2f6daec15e636c4bca5f67b7c22a5049d9ca
AMB <a.michael.boyle@gmail.com>
2023-11-23 22:45:04 -0500

    removed column title prop from task creation component

--- my-app/components/column/column.jsx
+++ my-app/components/column/column.jsx
@@ -80,7 +80,6 @@ export default function Column({
         <TaskCreation
           tasks={tasks}
           setToggleTaskCreation={setToggleTaskCreation}
-          columnTitle={columnTitle}
         />
       )}
     </div>
